62dc47310f372a4757ec5397aac2edac154bbf2d
Junhyun An <alloy0301@gmail.com>
2024-04-24 14:08:34 +0900

    Add : Network Method

--- 2a. Crytocurrency Intuition/hadcoin.py
+++ 2a. Crytocurrency Intuition/hadcoin.py
@@ -107,6 +107,19 @@ class Blockchain:
         max_length = len(self.chain)
         for node in network :
             response = requests.get(f'http://{node}/get_chain')  # get_chain 요청의 응답을 얻음
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+
+        return False
+
 
 
 # Part 2 = Mining our Blockhain
@@ -114,6 +127,10 @@ class Blockchain:
 
 # Creating a Web App
 app = Flask(__name__)
+
+# Creating an address for the node on Port 5000
+node_address = str(uuid4()).replace('-','')
+
 
 # Creating a Blockchain
 blockchain = Blockchain()
@@ -126,13 +143,15 @@ def mine_block():
     previous_proof = previous_block['proof']
     proof = blockchain.proof_of_work(previous_proof)
     previous_hash = blockchain.hash(previous_block)
+    blockchain.add_transaction(sender= node_address, receiver='junhyun', amount=10)
     block = blockchain.create_block(proof, previous_hash)
 
     response = {'message': 'Conratulations, you just mine a block!!',
                 'index': block['index'],
                 'timestamp': block['timestamp'],
                 'proof': block['proof'],
-                'previous_hash': block['previous_hash']}
+                'previous_hash': block['previous_hash'],
+                'transactions': block['transactions']}
 
     return jsonify(response), 200
 
@@ -157,7 +176,49 @@ def is_valid():
     return jsonify(response), 200
 
 
+# Adding a new transaction to the Blockchain
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    json = request.get_json()
+    transaction_keys = ['sender', 'receiver', 'amount']
+    if not all (key in json for key in transaction_keys):
+        return 'Some elements of the transaction are missing', 400
+
+    index = blockchain.add_transaction(json['sender'], json['receiver'], json['amount'])
+    response = {'message': f'This transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
+
 # Part 3 - Decentrallizing our Blockchain
+
+# Connecting new nodes
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    # 네트워크에 있는 모든 노드와 새로운 노드를 연결
+    nodes = json.get('nodes')
+    if nodes is None :
+        return 'No nodes provided', 400
+
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes now connected. The hadcoin Blockchain now contains the following nodes',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()  # chain을 교체해줌
+    if is_chain_replaced:
+        response = {'message': 'The node had different chains so the chain was replaced by the longest chain',
+                    'new_chain': blockchain.chain}
+    else:
+        response = {'message': 'All good. The chain is the largest one',
+                    'actual_chain': blockchain.chain}
+
+    return jsonify(response), 200
+
 
 # Running the app
 if __name__ == '__main__':
